5db34c4b2123b0ac033463c8fbbf330b93e4da09
Ashish Kumar Saha <85429920+risingstar-bit@users.noreply.github.com>
2023-01-31 17:39:10 +0530

    I added News App

---
+++ NewsApp/app/src/main/java/com/example/newsapp/ui/home/HomeFragment.java
@@ -0,0 +1,41 @@
+package com.example.newsapp.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.newsapp.R;
+import com.example.newsapp.WebViewController;
+import com.example.newsapp.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        WebView webView=root.findViewById(R.id.bbcWebView);
+        webView.loadUrl("https://digitalsevacsp.com/CSP/index.php");
+        webView.setWebViewClient(new WebViewController());
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
